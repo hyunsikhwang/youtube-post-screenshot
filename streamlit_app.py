eb41f96c547cdfacbebbec08ec00052fc81a3471
Hyunsik Hwang <gomting@naver.com>
2025-12-06 16:43:51 +0900

    playwright

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import os
+os.system("playwright install")
+os.system("playwright install chromium")
+os.system("playwright install-deps")
+
+import streamlit as st
+from playwright.sync_api import Playwright, sync_playwright, expect
